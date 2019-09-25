86be491b9736098191f9fe719ffc65c5839075bf
pkpsyder007 <pkpsyder007@gmail.com>
2019-09-25 16:40:37 +0530

    Email template editted

--- Email.tempate.js
+++ Email.tempate.js
@@ -303,7 +303,6 @@ module.exports = emailBody = (link) => (
     </style>
   </head>
   <body class="">
-    <span class="preheader">This is preheader text. Some clients will show this text as a preview.</span>
     <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="body">
       <tr>
         <td>&nbsp;</td>
